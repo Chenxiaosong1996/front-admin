e8ccc94e484c88fc498a2efdfedae5152e63a7f3
陈小松 <Chen.dezhen@outlook.com>
2022-03-01 17:48:33 +0800

    chore
    更新富文本的附件上传功能

--- src/app/shared/components/editor.md.ts
+++ src/app/shared/components/editor.md.ts
@@ -160,7 +160,9 @@ export class EditorMdComponent implements AfterViewInit, ControlValueAccessor {
     dialogDraggable: true, // 设置弹出层对话框不可拖动，全局通用，默认为true
     dialogMaskOpacity: 0.45, // 设置透明遮罩层的透明度，全局通用，默认值为0.1
     dialogMaskBgColor: '#000', // 设置透明遮罩层的背景颜色，全局通用，默认为#fff
-    imageUpload: false,
+    imageUpload: true,
+    imageFormats: ['jpg', 'jpeg', 'gif', 'png', 'bmp', 'webp'],
+    imageUploadURL: environment.api[environment.api.default].baseUrl + '/fileOSS/editoruploader',
   };
   private onChange = (_: any) => { };
   private onTouched = () => { };
